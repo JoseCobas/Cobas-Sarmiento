3d3f3ad31dc859c6c5f872e0ca7cb872a5aabe88
Jose Cobas <julgranen7@gmail.com>
2020-12-15 22:04:42 +0100

    manage js

--- admin/js/manage-posts.js
+++ admin/js/manage-posts.js
@@ -1,9 +1,37 @@
-/* async function fetchallpuns(){
+ async function fetchallpuns(){
     try{
-        let response = await fetch('');
-        let data     = resizeByponse.json();
+        let response = await fetch('http://localhost:3000/posts');
+        let data     = await response.json();
+
+        /* 
+                <li><strong>Title</strong> </li>
+                <li><strong>Author</strong></li>
+                <li><strong>Datum</strong>  </li>
+                <li><strong>Content</strong>
+        */
+
+        console.log(data);
+
+         let punsHtml = "";
+        for(let pun of data){
+            console.log(pun.author);
+            console.log(pun);
+
+            punsHtml += `<li>Title: ${pun.title} </li> <br> `;
+
+            let fullDatum = new Date(pun.date);                
+            punsHtml += `<li>Author: ${pun.author} | <span style= "color: lila;" > ${fullDatum.getFullYear()}-${fullDatum.getMonth()}-${fullDatum.getDate()} </span> </li> <br>`;
+            punsHtml += `<li>Content: ${pun.content} </li>`;
+            punsHtml += `<li>tags: ${pun.tags} </li>`;
+        }
+ 
 
 
-        let pun
+        document.getElementById('ul').innerHTML = punsHtml;
+        
+    }catch(message){
+        throw new Error(message);
     }
-} */
+    
+} 
+fetchallpuns();
